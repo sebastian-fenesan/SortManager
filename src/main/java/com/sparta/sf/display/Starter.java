cd4339d5b1d38c11fcc0c7d1f2e5d6be2e7f8b29
Sebastian Fenesan <SFenesan@spartaglobal.com>
2020-03-10 16:47:45 +0000

    Fix exception

--- src/main/java/com/sparta/sf/display/Starter.java
+++ src/main/java/com/sparta/sf/display/Starter.java
@@ -5,11 +5,15 @@ import com.sparta.sf.manager.SortManager;
 
 public class Starter {
 
-    public static void main(String[] args) throws SorterException {
+    public static void main(String[] args) {
 
         int[] testArray = {5,3,-4,6,4,20,0,1,2,1};
         SortManager sortManager = new SortManager();
-        sortManager.runSorter(testArray);
+        try {
+            sortManager.runSorter(testArray);
+        } catch (SorterException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
